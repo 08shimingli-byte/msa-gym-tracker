d0a9d12339259a3e8dbcaf52a78029c9b0b25597
Shiming <08shimingli@gmail.com>
2026-07-04 13:52:35 +1200

    chore: configure services

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,23 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+using GymTracker.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(opt =>
+    opt.UseSqlite("Data Source=gymtracker.db"));
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddCors(options =>
+    options.AddDefaultPolicy(policy =>
+        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+using (var scope = app.Services.CreateScope())
+    scope.ServiceProvider.GetRequiredService<AppDbContext>().Database.EnsureCreated();
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
+app.MapOpenApi();
+app.MapScalarApiReference();
+app.UseCors();
 app.MapControllers();
-
 app.Run();
